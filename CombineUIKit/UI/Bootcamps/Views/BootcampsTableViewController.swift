8af4519179f17c6dcc547f1a13dad9701cc45d0d
Roberto Rojo <rrojo.va@gmail.com>
2022-11-03 04:54:11 +0100

    Problem with cell name

--- CombineUIKit/UI/Bootcamps/Views/BootcampsTableViewController.swift
+++ CombineUIKit/UI/Bootcamps/Views/BootcampsTableViewController.swift
@@ -47,7 +47,7 @@ class BootcampsTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
 
         // Configure the cell...
         var content = cell.defaultContentConfiguration()
