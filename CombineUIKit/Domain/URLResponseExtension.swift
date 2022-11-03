69f3e700497bd8f682db666f8c0b796700a7db74
Roberto Rojo <rrojo.va@gmail.com>
2022-11-03 04:33:13 +0100

    Server call to get bootcamps with combine inside BootcampsViewModel

--- CombineUIKit/Domain/URLResponseExtension.swift
+++ CombineUIKit/Domain/URLResponseExtension.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Roberto Rojo Sahuquillo on 3/11/22.
 //
+import Foundation
 
 extension URLResponse {
     func getStatusCode() -> Int? {
--- CombineUIKit/UI/Bootcamps/ViewModels/BootcampsViewModel.swift
+++ CombineUIKit/UI/Bootcamps/ViewModels/BootcampsViewModel.swift
@@ -13,6 +13,35 @@ final class BootcampsViewModel: ObservableObject{
     @Published var bootcamps: [Bootcamp] = Array<Bootcamp>()
     var subcribers = Set<AnyCancellable>()
     
-    
+    func loadBotcamps() {
+        
+        let url = URL(string: "https://dragonball.keepcoding.education/api/data/bootcamps")
+        var request: URLRequest = URLRequest(url: url!)
+        
+        request.httpMethod = "GET"
+        
+        URLSession.shared.dataTaskPublisher(for: request)
+            .tryMap {
+                guard let response = $0.response as? HTTPURLResponse,
+                      response.getStatusCode() == 200 else {
+                    throw URLError(.badServerResponse)
+                }
+                return $0.data
+            }
+            .decode(type: [Bootcamp].self, decoder: JSONDecoder())
+            .receive(on: DispatchQueue.main)
+            .sink { completion in
+                switch completion{
+                case .failure(let error):
+                    print("Error: \(error)")
+                case .finished:
+                    print("Success")
+                }
+            } receiveValue: { data in
+                self.bootcamps = data
+            }
+            .store(in: &subcribers)
+
+    }
     
 }
